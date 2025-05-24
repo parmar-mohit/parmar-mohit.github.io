a41c01e89d1c7e6658a036eb8b495bc4786adf28
parmar-mohit <mohitparmar8080@gmail.com>
2025-05-24 18:54:07 +0530

    Removed Static Export Property

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
+//   output: 'export',
 //   assetPrefix: 'https://parmar-mohit.github.io/',
   typescript: {
     ignoreBuildErrors: true,
